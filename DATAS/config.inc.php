59e69a519e991257209ad11313b0ccf3fd004a67
xech <admin@omnispace.fr>
2019-04-12 15:47:47 +0200

    Initialize DATAS folder

--- DATAS/config.inc.php
+++ DATAS/config.inc.php
@@ -1,11 +1,11 @@
 <?php
 //CONNEXION À LA BDD
 define("db_host", "localhost");
-define("db_login", "test");
-define("db_password", "test");
-define("db_name", "agora");
+define("db_login", "");
+define("db_password", "");
+define("db_name", "");
 
 //ESPACE DISQUE / NB USERS / SALT
 define("limite_espace_disque", "10737418240");
 define("limite_nb_users", "10000");
-define("AGORA_SALT", "1429d70f");
+define("AGORA_SALT", "");
