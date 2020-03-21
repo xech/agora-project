0430cfbeb78e8e592dcc28ec93f78f99361b2787
xech <admin@omnispace.fr>
2020-03-21 10:37:36 +0100

    Update Agora-Project v3.7.0

--- DATAS/config.inc.php
+++ DATAS/config.inc.php
@@ -1,11 +1,11 @@
 <?php
 //CONNEXION À LA BDD : CONSTANTES VIDES PAR DEFAUT!
-define("db_host", "");
-define("db_login", "");
-define("db_password", "");
-define("db_name", "");
+define("db_host", "localhost");
+define("db_login", "test");
+define("db_password", "test");
+define("db_name", "test");
 
 //ESPACE DISQUE / NB USERS / SALT
-define("limite_espace_disque", "");
-define("limite_nb_users", "");
+define("limite_espace_disque", "5368709120");
+define("limite_nb_users", "50");
 define("AGORA_SALT", "");
